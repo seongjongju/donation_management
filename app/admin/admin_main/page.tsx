ae422c3bb231e9f9d7f4398c45a8d413ab2afc25
seongjongju <sjj990480@gmail.com>
2026-06-08 14:46:23 +0900

    페이지 자동생성의 경우 DB를 거쳐야함

--- app/admin/admin_main/page.tsx
+++ app/admin/admin_main/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const AdminMainPage = () => {
     return (
-        <div>
+        <main className='admin-main'>
             
-        </div>
+        </main>
     );
 };
 
---
+++ app/admin/inc/lnb/Lnb.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Lnb = () => {
+    return (
+        <div id='lnb'>
+            <ul className='lnb__list'>
+                
+            </ul> {/* .lnb__list : end */}
+        </div>
+    );
+};
+
+export default Lnb;
