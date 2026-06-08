3c1218921032a3a370b9096accb0271e0ce17afd
seongjongju <sjj990480@gmail.com>
2026-06-08 15:02:59 +0900

    페이지 자동생성의 경우 DB를 거쳐야함

--- app/admin/inc/lnb/Lnb.tsx
+++ app/admin/inc/lnb/Lnb.tsx
@@ -1,11 +1,69 @@
-
+import Link from 'next/link';
 import React from 'react';
+
+const lnbs = [
+    {
+        id: "menu_0",
+        oneDepth: "회원관리",
+        href: "/admin/members"
+    },
+    {
+        id: "menu_1",
+        oneDepth: "일반기부금",
+        href: ""
+    },
+    {
+        id: "menu_2",
+        oneDepth: "사업별기부금",
+        twoDepths: [
+            {
+                id: "menu_2_depth_0",
+                twoDepth: "사업1",
+                href: "/admin/business_donation/menu_2_depth_0"
+            },
+            {
+                id: "menu_2_depth_1",
+                twoDepth: "사업2",
+                href: "/admin/business_donation/menu_2_depth_0"
+            },
+        ],
+    }
+];
 
 const Lnb = () => {
     return (
         <div id='lnb'>
             <ul className='lnb__list'>
-                
+                {
+                    lnbs.map((lnb) => {
+                        return (
+                            <li key={lnb.id}>
+                                <Link href={lnb.href ? lnb.href : "#self"}>
+                                    {lnb.oneDepth}
+                                </Link>
+                                {
+                                    lnb.twoDepths && 
+                                    (
+                                        <ul>
+                                            {
+                                                lnb.twoDepths?.map((depth) => {
+                                                    return (
+                                                        <li key={depth.id}>
+                                                            <Link href={depth.href}>{depth.twoDepth}</Link>
+                                                        </li>
+                                                    )
+                                                })     
+                                            }
+                                            <li>
+                                                <button>사업 추가</button>
+                                            </li>
+                                        </ul>
+                                    )
+                                }
+                            </li>
+                        )
+                    })
+                }
             </ul> {/* .lnb__list : end */}
         </div>
     );
